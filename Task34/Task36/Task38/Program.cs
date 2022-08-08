c4206e0626f1f16c2e984f5c38c859d7818d45c4
andrey_orexxx <KO.orexxx@yandex.ru>
2022-08-08 22:38:34 +0300

    Задача 38 решена

---
+++ Task34/Task36/Task38/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 38 
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива. 
+ 
+double[] arreyNumbers = new double[10]; 
+ 
+for (int i = 0; i < arreyNumbers.Length; i++) 
+{ 
+    arreyNumbers[i] = new Random().Next(1, 10); 
+    Console.Write(arreyNumbers[i] + " "); 
+} 
+ 
+double maxNumber = arreyNumbers[0]; 
+double minNumber = arreyNumbers[0]; 
+ 
+for (int i = 1; i < arreyNumbers.Length; i++) 
+{ 
+    if (maxNumber < arreyNumbers[i]) 
+    { 
+        maxNumber = arreyNumbers[i]; 
+    } 
+    if (minNumber > arreyNumbers[i]) 
+    { 
+        minNumber = arreyNumbers[i]; 
+    } 
+} 
+ 
+double decision = maxNumber - minNumber; 
+
+Console.WriteLine();
+ 
+Console.WriteLine($"Разница между максимальным ({maxNumber}) и минимальным ({minNumber}) элементами: {decision}");
